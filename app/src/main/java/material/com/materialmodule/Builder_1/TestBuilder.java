0d65dd599e57459d3618257f7e346a6c81bbeba0
袁兵 <yuanbing@qccr.com>
2017-04-25 16:12:05 +0800

    builder-1

--- app/src/main/java/material/com/materialmodule/Builder_1/TestBuilder.java
+++ app/src/main/java/material/com/materialmodule/Builder_1/TestBuilder.java
@@ -3,7 +3,8 @@ package material.com.materialmodule.Builder_1;
 /**
  * @author: yuanbing
  * @date: 2017-04-25 15:27
- * @description 建造者模式
+ * @description 建造者模式:主要功能是基本方法的调用的顺序的安排，也就是这些基本的方法已经实现了，通俗的说就是
+ * 零件的装配，顺序不同产生的对象也不同，而工厂方法的重点是创建，创建零件是它的主要的职责，组装顺序则不是它关心的
  */
 public class TestBuilder {
     public static void main(String[] args) {
